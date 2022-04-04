feeb6bb341e69dc09069fc2966ff366f182ca86b
Fernando Martins Ferreira <fernando.ferreira@basis.com.br>
2022-04-04 10:58:50 -0300

    Criando novo microserviço principal para substituir o atual

---
+++ service/src/main/java/br/com/fmf/sistematrocaitens/service/SistemaDeTrocaDeItensServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.fmf.sistematrocaitens.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeTrocaDeItensServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeTrocaDeItensServiceApplication.class, args);
+	}
+
+}
---
+++ service/src/test/java/br/com/fmf/sistematrocaitens/service/SistemaDeTrocaDeItensServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fmf.sistematrocaitens.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeTrocaDeItensServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
